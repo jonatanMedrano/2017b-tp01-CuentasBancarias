b68927f9bb58de7a1fd277fbd105b738534668a4
Francisco Figueroa <figueroafe@gmail.com>
2017-09-06 22:15:08 -0300

    Creacion de la clase
    Se crea la clase CuentaBancria con sus metodos.

--- src/cuenta/Bancaria/CuentaBancaria.java
+++ src/cuenta/Bancaria/CuentaBancaria.java
@@ -3,5 +3,35 @@ package cuenta.Bancaria;
 public class CuentaBancaria {
 	private int numeroCuenta;
 	private double saldo;
-
+	
+	/**
+	 * Constructor de la clase cuenta bancaria
+	 * @param numeroCuenta
+	 * @param saldo
+	 */
+	public CuentaBancaria(int numeroCuenta, double saldo) {
+		this.numeroCuenta = numeroCuenta;
+		this.saldo = saldo;
+	}
+	
+	/**
+	 * Acredito el monto a la cuenta destino y debita de la cuenta de origen
+	 * @param monto
+	 * @param cuentaDestino
+	 */
+	
+	public void transferenciaBancaria(double monto, CuentaBancaria cuentaDestino){
+		if(this.saldo <= monto){
+			cuentaDestino.saldo += monto;
+			this.saldo -= monto;
+		}
+	}
+	
+	/**
+	 * Muestra el salto actual
+	 * @return
+	 */
+	public double consultarSaldo(){
+		return this.saldo;
+	}
 }
