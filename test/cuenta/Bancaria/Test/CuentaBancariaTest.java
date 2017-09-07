967083021ec196d779b5ac19999a6e8920e2444b
Jonatan Medrano <jonatan.medrano@gmail.com>
2017-09-06 22:23:36 -0300

    ROJO: Primera parte del test, primera prueba de forzado de falla

--- test/cuenta/Bancaria/Test/CuentaBancariaTest.java
+++ test/cuenta/Bancaria/Test/CuentaBancariaTest.java
@@ -1,5 +1,22 @@
 package cuenta.Bancaria.Test;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import cuenta.Bancaria.CuentaBancaria;
+
 public class CuentaBancariaTest {
 
+	
+	@Before
+	public void setUp(){
+		CuentaBancaria cuentaOrigen = new CuentaBancaria(12345678, 100);
+		CuentaBancaria cuentaDestino = new CuentaBancaria(12345678, 10);
+	}
+	
+	@Test
+	public void queHayaMontoCeroEnLaCuenta(){
+		Assert.fail();
+	}
 }
