955648b37f193147becd41d9ba3f4960cc51b16f
Jonatan Medrano <jonatan.medrano@gmail.com>
2017-09-06 22:55:39 -0300

    VERDE: se comprueba el caso con cuenta sin saldo

--- test/cuenta/Bancaria/Test/CuentaBancariaTest.java
+++ test/cuenta/Bancaria/Test/CuentaBancariaTest.java
@@ -7,16 +7,29 @@ import org.junit.Test;
 import cuenta.Bancaria.CuentaBancaria;
 
 public class CuentaBancariaTest {
-
+	
+	CuentaBancaria cuentaOrigen;
+	CuentaBancaria cuentaDestino;
 	
 	@Before
 	public void setUp(){
-		CuentaBancaria cuentaOrigen = new CuentaBancaria(12345678, 100);
-		CuentaBancaria cuentaDestino = new CuentaBancaria(12345678, 10);
+		 cuentaOrigen = new CuentaBancaria(12345678, 100);
+		 cuentaDestino = new CuentaBancaria(12345678, 10);
 	}
-	
+
+	/**
+	 * Compruebo que la cuenta de origen quedo en cero
+	 */
 	@Test
-	public void queHayaMontoCeroEnLaCuenta(){
-		Assert.fail();
+	public void queHayaMontoCeroEnLaCuentaOrigen(){
+		//Tranfiero todo el saldo a la cuenta destino
+		cuentaOrigen.transferenciaBancaria(100, cuentaDestino);
+		
+		//Compruebo que el saldo quede en 0 en la cuenta de origen
+		Assert.assertEquals(0,cuentaOrigen.consultarSaldo(),0);
 	}
+
+
+
+	
 }
