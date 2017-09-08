f34ae29d08385c173524b1ae479e2268f0e47751
Jonatan Medrano <jonatan.medrano@gmail.com>
2017-09-08 15:19:29 -0300

    AZUL: Se agrega validacion para evitar transferencias de mas de 100.000 pesos por dia

--- src/cuenta/Bancaria/CuentaBancaria.java
+++ src/cuenta/Bancaria/CuentaBancaria.java
@@ -3,9 +3,10 @@ package cuenta.Bancaria;
 public class CuentaBancaria {
 	private int numeroCuenta;
 	private double saldo;
-	
+
 	/**
 	 * Constructor de la clase cuenta bancaria
+	 * 
 	 * @param numeroCuenta
 	 * @param saldo
 	 */
@@ -13,25 +14,29 @@ public class CuentaBancaria {
 		this.numeroCuenta = numeroCuenta;
 		this.saldo = saldo;
 	}
-	
+
 	/**
 	 * Acredito el monto a la cuenta destino y debita de la cuenta de origen
+	 * 
 	 * @param monto
 	 * @param cuentaDestino
 	 */
-	
-	public void transferenciaBancaria(double monto, CuentaBancaria cuentaDestino){
-		if(this.saldo >= monto){
-			cuentaDestino.saldo += monto;
-			this.saldo -= monto;
+
+	public void transferenciaBancaria(double monto, CuentaBancaria cuentaDestino) {
+		if (monto < 100000) {
+			if (this.saldo >= monto) {
+				cuentaDestino.saldo += monto;
+				this.saldo -= monto;
+			}
 		}
 	}
-	
+
 	/**
 	 * Muestra el saldo actual
+	 * 
 	 * @return
 	 */
-	public double consultarSaldo(){
+	public double consultarSaldo() {
 		return this.saldo;
 	}
 }
